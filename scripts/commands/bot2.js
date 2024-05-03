5fe5bf7d4e1d87992b16820f7f26376849369c68
RAKIB CHOWDHURY <152077471+SYSTEM-ERROR-KING@users.noreply.github.com>
2024-05-03 19:47:41 +0600

    Update bot2.js

--- scripts/commands/bot2.js
+++ scripts/commands/bot2.js
@@ -28,6 +28,10 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
      return api.sendMessage("Nh ami চুপ থাকবো না.😼 রাকিব বস আমারে কথা বলতে বলছে🤭", threadID);
    };
 
+  if ((event.body.toLowerCase() == "Who is Rakib") || (event.body.toLowerCase() == "Who is your admin") || (event.body.toLowerCase() == "Who is your owner") || (event.body.toLowerCase() == "bot kar") || (event.body.toLowerCase() == "Rakib k") || (event.body.toLowerCase() == "Rakib koi")) {
+     return api.sendMessage("~ আসসালামু আলাইকুম সবাই কেমন আছেন আসা করি আল্লাহর রহমতে অনেকটাই ভালো আছেন। বট বিষয়ে কোন প্রয়োজন হলে আমার বস এর  ইনবক্সে বলেন। ধন্যবাদ  😐://www.facebook.com/SYSTEM.ERROR.KING :))", threadID);
+   }; 
+
    if ((event.body.toLowerCase() == "Raisa") || (event.body.toLowerCase() == "রাইসা")) {
      return api.sendMessage("খবরদার কেউ এই নাম দরে ডাক দিবানা এটা আমার বস রাকিব এর বউ এর নাম..!😠🥰⛏️", threadID);
    };
